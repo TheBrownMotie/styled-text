10caa0985f3f6a8f8403d47f1510ab8b49c47b1c
BrownMotie <>
2026-05-21 17:29:52 -0700

    add exports to react

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,1 +1,5 @@
 export { StyledText } from "./StyledText";
+
+export { TextStyler, TextStylerRegexRule, TextStylerRule, htmlTag } from "@brownmotie/styled-text";
+
+export type { RuleType } from '@brownmotie/styled-text';
